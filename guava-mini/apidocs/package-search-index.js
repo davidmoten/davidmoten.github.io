d939bc08d1a928da46764f598ceaf6cc81bc83fb
Dave Moten <davidmoten@gmail.com>
2023-06-27 16:54:23 +1000

    update site reports

---
+++ guava-mini/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.davidmoten.guavamini"},{"l":"com.github.davidmoten.guavamini.annotations"}];updateSearchResults();
